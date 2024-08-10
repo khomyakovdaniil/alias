99c0c1cc6f963b16077f536d5bdcc9175af39897
KhomyakovDaniil <daniil.h@appomart.com>
2024-08-10 18:07:05 +0300

    Initial Commit

---
+++ Alias/AliasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasApp.swift
+//  Alias
+//
+//  Created by  Даниил Хомяков on 10.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AliasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
